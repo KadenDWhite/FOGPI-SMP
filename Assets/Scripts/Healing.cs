7c34b85fd3a35ee371c7cd384404d0f5fe07dd5f
Kaden D. White <kadendw23@gmail.com>
2024-05-07 14:02:01 -0500

    Wild Healers and Hierarchy space

--- Assets/Scripts/Healing.cs
+++ Assets/Scripts/Healing.cs
@@ -5,72 +5,62 @@ using SuperPupSystems.Helper;
 
 public class Healing : MonoBehaviour
 {
-    public string allyTag; // Tag of the allies that can be healed
-    public int healedAmount = 10;
+    public string allyTag; 
+    public int healedAmount = 2; // Amount to heal per second
+    public float healCooldown = 2f; // Cooldown between healing same ally
     public ParticleSystem healParticles;
-    public Animator healAnimator; // Reference to the Animator component
+    public Animator healAnimator;
 
-    private int numAlliesAlive;
-
-    void Start()
-    {
-        // Count the number of allies alive at the start
-        GameObject[] allies = GameObject.FindGameObjectsWithTag(allyTag);
-        numAlliesAlive = allies.Length;
-    }
+    private float lastHealTime;
+    private Health lastHealedAlly;
 
     void OnTriggerEnter(Collider other)
     {
         Health health = other.GetComponent<Health>();
 
-        // Check if the collider has a Health component and is an ally
-        if (health && other.CompareTag(allyTag))
+        if (health && other.CompareTag(allyTag) && other.gameObject != gameObject)
         {
-            // Check if the current object is not the healer itself
-            if (other.gameObject != gameObject)
+            // Check if the ally is not currently being healed by another healer
+            if (lastHealedAlly != health || Time.time - lastHealTime >= healCooldown)
             {
-                // Check if there are any lower health allies nearby
-                Health lowestHealthAlly = FindLowestHealthAlly(allyTag);
-                if (lowestHealthAlly != null && lowestHealthAlly != health)
-                {
-                    // Heal the lowest health ally
-                    lowestHealthAlly.Heal(healedAmount);
-                }
-                else
-                {
-                    // No more allies to heal, stop the animator
-                    StopAnimator();
-                }
+                HealAlly(health);
             }
         }
     }
 
-    // Find the lowest health ally within the specified tag
-    Health FindLowestHealthAlly(string tag)
+    void HealAlly(Health allyHealth)
     {
-        GameObject[] allies = GameObject.FindGameObjectsWithTag(tag);
-        Health lowestHealthAlly = null;
-        float lowestHealth = float.MaxValue;
-
-        foreach (GameObject ally in allies)
-        {
-            Health allyHealth = ally.GetComponent<Health>();
-            if (allyHealth && allyHealth.currentHealth < lowestHealth)
-            {
-                lowestHealth = allyHealth.currentHealth;
-                lowestHealthAlly = allyHealth;
-            }
-        }
-
-        return lowestHealthAlly;
+        lastHealedAlly = allyHealth;
+        lastHealTime = Time.time;
+        StartCoroutine(HealOverTime(allyHealth));
     }
 
-    // Stop the animator
-    void StopAnimator()
+    IEnumerator HealOverTime(Health allyHealth)
+    {
+        while (allyHealth.currentHealth < allyHealth.maxHealth)
+        {
+            allyHealth.Heal(healedAmount);
+            yield return new WaitForSeconds(1f); // Heal every second
+        }
+    }
+
+    // Stop the animator and particles
+    void StopAnimatorAndParticles()
     {
         if (healAnimator != null)
         {
             healAnimator.enabled = false;
         }
+        if (healParticles != null)
+        {
+            healParticles.Stop();
+        }
+    }
+
+    // Call this function when the healing process needs to stop
+    public void StopHealing()
+    {
+        StopCoroutine("HealOverTime");
+        StopAnimatorAndParticles();
     }
 }
